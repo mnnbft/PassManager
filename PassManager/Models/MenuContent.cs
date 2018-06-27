91f83698061ce1e4b741435890fae05e09c95b90
mnnbft <0x114514@gmail.com>
2018-06-28 01:10:47 +0900

    同期

---
+++ PassManager/Models/MenuContent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace PassManager.Models
+{
+    public sealed class MenuContent : BindableBase
+    {
+        public static MenuContent Instance { get; }
+            = new MenuContent();
+        private MenuContent() { }
+
+        public enum MenuButtonItem
+        {
+            View,
+            Generation,
+            Setting,
+        }
+        public Dictionary<MenuButtonItem, string> menuButtonDictionary =
+            new Dictionary<MenuButtonItem, string>()
+            {
+                { MenuButtonItem.View, "表示画面" },
+                { MenuButtonItem.Generation, "パスワード作成" },
+                { MenuButtonItem.Setting, "設定" },
+            };
+        public string[] MenuButtonItems
+        {
+            get { return menuButtonDictionary.Select(i => i.Value).ToArray(); }
+        }
+    }
+}
--- PassManager/ViewModels/PassManagerViewModel.cs
+++ PassManager/ViewModels/PassManagerViewModel.cs
@@ -5,10 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using Prism.Mvvm;
 using Prism.Commands;
+using PassManager.Models;
 
 namespace PassManager.ViewModels
 {
     public class PassManagerViewModel : BindableBase
     {
+        public string[] MenuButtonItems
+        {
+            get { return MenuContent.Instance.MenuButtonItems; }
+        }
     }
 }
